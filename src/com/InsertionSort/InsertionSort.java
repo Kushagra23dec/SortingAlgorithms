e85e43e5bd7f7419529ca76ecce586a40ba8b998
Kushagra Upadhyaya <kushagra23dec@gmail.com>
2022-04-03 22:17:37 +0530

    cc

--- src/com/InsertionSort/InsertionSort.java
+++ src/com/InsertionSort/InsertionSort.java
@@ -1,5 +1,6 @@
 package com.InsertionSort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,32 +8,32 @@ import java.util.Scanner;
 public class InsertionSort {
     public static void main(String[] args) {
         int i,j,k;
-        int [] a = new int[5];
-
+       // int [] a = new int[5];
+        ArrayList<Integer> a = new ArrayList<>();
         Scanner s = new Scanner(System.in);
         System.out.println("Enter value of n: ");
         int n = s.nextInt();
         System.out.println("Enter the Elements in Array: ");
         for(int x = 0;x<n;x++)
-            a[x] = s.nextInt();
+            a.add(x,s.nextInt());
 
 
 
 
-        System.out.println("UnSorted Array: "+ Arrays.toString(a));
+        System.out.println("UnSorted Array: "+ a);
 
-        for(j=1;j<a.length;j++){
-            k = a[j]; // assign value of key at jth position
+        for(j=1;j<a.size();j++){
+            k = a.get(j); // assign value of key at jth position
             i = j-1; // value of i will always be one less than j
-            while(i>=0 && a[i] > k){
-                a[i+1] = a[i];
+            while(i>=0 && a.get(i) > k){
+                a.set(i+1,a.get(i)) ;
                 i = i-1;
             }
-            a[i+1] = k;
+            a.set(i+1,k);
 
         }
 
-        System.out.println("Sorted Array: "+ Arrays.toString(a));
+        System.out.println("Sorted Array: "+ a);
 
     }
 }
